977f2ca707c34483a2e712834b1677fc7a7f545b
KyeongsooLee <leeksca26@gmail.com>
2023-03-29 19:10:25 -0400

    Second commit

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ MenuBtn.addEventListener('click', function(e) {
     document.querySelector('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('fa-xmark')
 })
+
+
+// Typing Effect
+
+let typed = new Typed('.auto-input',{
+    strings: ['Front-End Developer!', 'Freelancer!', 'UI Designer!', 'YouTuber!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
