760a279620edb300091db69bf6c0f3ceb81c2077
KyeongsooLee <leeksca26@gmail.com>
2023-04-19 22:03:28 -0400

    updated contact

--- script.js
+++ script.js
@@ -38,3 +38,49 @@ window.addEventListener('scroll', function(){
         }
     });
 });
+
+
+// EmailJS initialization
+emailjs.init('jl75OLLxs41kW2E_G');
+
+// Function to handle form submission
+function sendEmail(event) {
+  // Prevent the form from submitting normally
+  event.preventDefault();
+
+  // Get the form data
+  const name = document.getElementById('name').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const subject = document.getElementById('subject').value.trim();
+  const message = document.getElementById('msg').value.trim();
+
+  // Check if all fields are filled out
+  if (!name || !email || !subject || !message) {
+    alert('Please fill out all fields.');
+    return;
+  }
+
+  // Prepare the email template parameters
+  const templateParams = {
+    name: name,
+    email: email,
+    subject: subject,
+    message: message
+  };
+  
+  // Send the email using EmailJS
+  emailjs.send('service_cqecci6', 'template_r918mm8', templateParams)
+  .then(function(response) {
+    alert('Email sent successfully!');
+    // Clear the form fields
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('subject').value = '';
+    document.getElementById('msg').value = '';
+  }, function(error) {
+    alert('Failed to send email: ' + error);
+  });
+}
+
+// Add event listener to the form submit button
+document.getElementById('contact-form').addEventListener('submit', sendEmail);
